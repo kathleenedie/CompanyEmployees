05a49b8a431a9b171db266880ca02afe1bdd81be
Kathleen Edie <kathleen.edie@arnoldclark.com>
2022-06-23 16:35:53 +0100

    add services to program.cs

---
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace CompanyEmployees.Extensions;
+
+public static class ServiceExtensions
+{
+    // In production AllowAnyOrigin should be changed to WithOrigins()
+    // In production AllowAnyMethod should be changed to WithMethods()
+    // Om production AllowAnyHeader should be changed to WIthHeaders()
+    public static void ConfigureCors(this IServiceCollection services) =>
+    services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+    });
+
+    // IIS configuration is required to deploy to IIS
+    public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+        });
+
+}
